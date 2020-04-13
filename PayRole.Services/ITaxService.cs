0ddcbab5be785aa7fe9305b52194f6270687ff4d
ChrisBlingz <cc.ironuma@gmail.com>
2020-04-13 15:38:00 +0100

    okay

---
+++ PayRole.Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayRole.Services
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
---
+++ PayRole.Services/Implementation/TaxService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayRole.Services.Implementation
+{
+    public class TaxService : ITaxService
+    {
+        private decimal taxRate;
+        private decimal tax;
+
+        public decimal TaxAmount(decimal totalAmount)
+        {
+            if(totalAmount <= 1042)
+            {
+                //Tax Free Rate
+                taxRate = 0.0m;
+                tax = totalAmount * taxRate;
+            }
+            else if (totalAmount > 1042 && totalAmount <= 3125)
+            {
+                //Basic tax Rate
+                taxRate = 0.20m;
+                //Income Tax
+                tax = (1042 * 0.0m) + ((totalAmount - 1042) * taxRate);
+            }
+            else if (totalAmount > 3125 && totalAmount <= 12500)
+            {
+                //Higher Tax rate
+                taxRate = 0.40m;
+                //income Tax
+                tax = (1042 * 0.0m) + ((3125 - 1042) * 0.20m) + ((totalAmount - 3125) * taxRate);
+            }
+            else if (totalAmount > 12500)
+            {
+                //Additional Tax rate
+                taxRate = 0.45m;
+                //income Tax
+                tax = (1042 * 0.0m) + ((3125 - 1042) * 0.20m) + ((12500 - 3125) * 0.40m) + ((totalAmount - 12500) * taxRate);
+            }
+
+            return tax;
+        }
+    }
+}
