6113a112eb3b3494b064b3846afce8135e540aa3
ChrisBlingz <cc.ironuma@gmail.com>
2020-04-13 15:59:51 +0100

    NIC Service and implimentation

---
+++ PayRole.Services/INationalInsuranceContributionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayRole.Services
+{
+    public interface INationalInsuranceContributionService
+    {
+        decimal NIContribution(decimal totalAmount);
+    }
+}
