2819374312ffa745d5b30c9639cad62d0d28858e
ChrisBlingz <cc.ironuma@gmail.com>
2020-04-13 13:51:33 +0100

    PayRole Interface and Implementation

---
+++ PayRole.Services/IPayRoleService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PayRole.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRole.Services
+{
+    public interface IPayRoleService
+    {
+        Task CreatAsync(PaymentRecord paymentRecord);
+        PaymentRecord GetById(int id);
+        IEnumerable<PaymentRecord> GetAll();
+        IEnumerable<SelectListItem> GetAllTaxYear();
+        decimal OvertimeHours(decimal hoursWorked, decimal contratualHours);
+        decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate);
+        decimal OvertimeRate(decimal hourlyRate);
+        decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours);
+        decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings);
+        decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal UnionFees);
+        decimal NetPay(decimal totalEarnings, decimal totalDeduction);
+    }
+}
---
+++ PayRole.Services/Implementation/PayRoleService.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PayRole.Entity;
+using PayRole.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRole.Services.Implementation
+{
+    public class PayRoleService : IPayRoleService
+    {
+        private decimal contractualEarnings;
+        private decimal overtimeHours;
+        private readonly ApplicationDbContext _context;
+
+        public PayRoleService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+           if(hoursWorked < contractualHours)
+            {
+                contractualEarnings = hoursWorked * hourlyRate;
+            }
+            else
+            {
+                contractualEarnings = contractualHours * hourlyRate;
+            }
+            return contractualEarnings;
+        }
+
+        public async Task CreatAsync(PaymentRecord paymentRecord)
+        {
+           await _context.PaymentRecords.AddAsync(paymentRecord);
+           await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll()
+        {
+            return _context.PaymentRecords.OrderBy(p => p.EmployeeId);
+        }
+
+        public IEnumerable<SelectListItem> GetAllTaxYear()
+        {
+            var allTaxYear = _context.TaxYears.Select(x => new SelectListItem
+            {
+                Text = x.YearOfTax,
+                Value = x.Id.ToString()
+            });
+
+            return allTaxYear;
+        }
+
+        public PaymentRecord GetById(int id)
+        {
+            return _context.PaymentRecords.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeduction)
+        {
+            return totalEarnings - totalDeduction;
+        }
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours)
+        {
+            return overtimeHours * overtimeRate;
+        }
+
+        public decimal OvertimeHours(decimal hoursWorked, decimal contratualHours)
+        {
+            if(hoursWorked <= contratualHours)
+            {
+                overtimeHours = 0.00m;
+            }
+            else if(hoursWorked > contratualHours)
+            {
+                overtimeHours = hoursWorked - contratualHours;
+            }
+
+            return overtimeHours;
+        }
+
+        public decimal OvertimeRate(decimal hourlyRate)
+        {
+            return hourlyRate * 1.5m;
+        }
+
+        public decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal UnionFees)
+        {
+            return tax + nic + studentLoanRepayment + UnionFees;
+        }
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings)
+        {
+            return overtimeEarnings + contractualEarnings;
+        }
+    }
+}
